4e5e6d77e5376eab694aa6518c278a914fe27a7b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-05 18:48:23 +0100

    set up the header file

--- hash_table.h
+++ hash_table.h
@@ -1,4 +1,22 @@
 #ifndef _HASH_TABLE_H_
 #define _HASH_TABLE_H_
 
+typedef struct hash_node_s
+{
+	char *word;
+	int frequency;
+	struct hash_node_s *next;
+} hash_node_t;
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	unsigned long int capacity;
+	hash_node_t **array;
+} hash_table_t;
+
+int hash_table_set(hash_table_t *ht, char *word, int frequency);
+hash_table_t *hash_table_create(unsigned long int size);
+void hash_table_print(hash_table_t *ht);
+
 #endif /* _HASH_TABLE_H_ */
